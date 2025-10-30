342e4524d123a076b3b9417daa97acefc1b5d525
azirmi <batu.mutlu321@gmail.com>
2025-10-30 16:44:27 +0300

    Sprint 1 frontend tamamlandı - React, Tailwind, Dark mode, API bağlantıları

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import AnalyticsPage from "./pages/AnalyticsPage";
+import AssistantsPage from "./pages/AssistantsPage";
+import { ThemeProvider } from "./context/ThemeContext";
+import { SearchProvider } from "./context/SearchContext";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider>
+      <SearchProvider>
+        <Router>
+          <div className="flex bg-gray-100 dark:bg-gray-900 dark:text-gray-100 transition-colors">
+            <Sidebar />
+            <div className="flex-1 ml-64 min-h-screen">
+              <Navbar />
+              <div className="p-4">
+                <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/analytics" element={<AnalyticsPage />} />
+                  <Route path="/assistants" element={<AssistantsPage />} />
+                </Routes>
+              </div>
+            </div>
+          </div>
+        </Router>
+      </SearchProvider>
+    </ThemeProvider>
   );
 }
 
